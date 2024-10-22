7190c7043dc53f2db97d321e1545602178cf6fc4
cnr <cnr@tenex.engineering>
2024-10-23 01:24:31 +0700

    update setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,19 @@
 
 import { includeIgnoreFile } from '@eslint/compat'
 import eslint from '@eslint/js'
-import path from 'path'
-import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import * as path from 'path'
+import tslint from 'typescript-eslint'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
-export default tseslint.config(
+export default tslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+  ...tslint.configs.strict,
+  ...tslint.configs.stylistic,
+  prettierConfig,
   includeIgnoreFile(gitignorePath),
 )
