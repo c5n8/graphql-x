355a087caa2cd24574c358cb9abf54f2ee0972d4
cnr <cnr@tenex.engineering>
2024-10-27 07:14:02 +0700

    add test

---
+++ src/utils/traillead.test.ts
@@ -0,0 +1,18 @@
+import { traillead } from '#app/utils/traillead.js'
+import { expect, test } from 'vitest'
+
+test('traillead', async () => {
+  let store = 0
+
+  const save = traillead(async (n: number) => {
+    await new Promise<void>((resolve) => setTimeout(resolve, 0))
+
+    store = n
+  })
+
+  await Promise.all([
+    save(1).then(() => expect(store).toBe(3)),
+    save(2).then(() => expect(store).toBe(0)),
+    save(3).then(() => expect(store).toBe(0)),
+  ])
+})
