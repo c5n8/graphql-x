83484cff6022e482528d753032f7804b507e9f6a
cnr <cnr@tenex.engineering>
2024-11-18 19:55:01 +0700

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '#app': fileURLToPath(new URL('./src', import.meta.url)),
+      '#documents': fileURLToPath(new URL('./documents', import.meta.url)),
     },
   },
 })
